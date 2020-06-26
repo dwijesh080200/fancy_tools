880ebc4079c61dd831b9dd6bcd88f8b74df4c6c2
dwijesh080200 <dwi.rag123@gmail.com>
2020-06-26 09:05:11 +0000

    [master] install 1.0

--- install.sh
+++ install.sh
@@ -1,1 +1,41 @@
 #!/bin/bash
+
+dir="~/src/fancy_tools"
+a="source $dir/.aliases"
+b="source $dir/fancy_functions.sh"
+c=$(<~/.bashrc)
+d="export PATH=$PATH:~/bin"
+
+# sourcer le ./aliases
+if grep -q "$a" <<< "$c"; then
+	echo "Fichier '.aliases'est deja dans le bashrc"
+else
+	echo source $dir/.aliases >> ~/.bashrc
+	echo "Fichier '.aliases' a etait ajouer dans le bashrc avec succes"
+fi
+
+#file fancy_functions.sh
+if grep -q "$b" <<< "$c"; then
+	echo "Fichier 'Fancy_functions.sh' est deja dans le bashrc"
+else
+	echo source $dir/fancy_functions.sh >> ~/.bashrc
+	echo "Fichier 'fancy_functions.sh' a etair ajouter dans le bashrc avec succes"
+fi
+
+#folder ~/bin
+if [[ -d ~/bin ]]
+then
+	echo "folder ~/bin existe deja"
+else
+	mkdir ~/bin
+	echo "folder ~/bin a etait ajouter avec succes"
+fi
+
+#PATH
+if grep -q "$d" <<< "$c"; then
+	echo "PATH est deja dans le bashrc"
+else
+	echo "export PATH=$PATH:~/bin" >> ~/.bashrc
+	echo "PATH a etait ajouter dans le bashrc avec succes"
+fi
+
