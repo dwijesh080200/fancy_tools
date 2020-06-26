101af2f4598fee3a363c4f6d37e7a8274aff5b3b
dwijesh080200 <dwi.rag123@gmail.com>
2020-06-26 08:26:03 +0000

    [master]

--- fancy_functions.sh
+++ fancy_functions.sh
@@ -1,1 +1,13 @@
-#!/bin/bash
+function gco
+{
+	inside_git_repo="$(git rev-parse --is-inside-work-tree 2>/dev/null)"
+
+	if [ "$inside_git_repo" ]; then
+	{
+		current_branch="$(git rev-parse --abbrev-ref HEAD)"
+		git commit -m "[$current_branch] $*"
+	}
+	else
+		echo "!!vous n'etes pas dans un projet git!!"
+	fi
+}
